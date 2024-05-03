858bac610399449a8da3a32c786eed97fa037040
Adebayo Yuusuf <adebayoayomide396@gmail.com>
2024-05-03 00:24:22 -0300

    adding the task display functionality

--- init.py
+++ init.py
@@ -1,17 +1,3 @@
-from win10toast import ToastNotifier
-import time
+from win11toast import toast
 
-from win10toast import ToastNotifier
-toaster = ToastNotifier()
-# toaster.show_toast("Hello World!!!",
-#                    "Python is 10 seconds awsm!",
-#                    icon_path="custom.ico",
-#                    duration=10)
-
-toaster.show_toast("Example two",
-                   "This notification is in it's own thread!",
-                   icon_path=None,
-                   duration=30,
-                   threaded=True)
-# Wait for threaded notification to finish
-while toaster.notification_active(): time.sleep(0.1)
+toast('Hello Python🐍', dialogue='Hello world')
